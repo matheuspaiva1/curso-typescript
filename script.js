928d03306be1eacf631bf9b24592467b1fc5b783
matheuspaiva1 <matheuspaiva0007@gmail.com>
2024-05-21 19:25:56 -0300

    types

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 "use strict";
-function toNumber(value) {
-    if (typeof value === 'number') {
-        return value;
-    }
-    else if (typeof value === 'string') {
-        return Number(value);
-    }
-    else {
-        const err = new Error("value deve ser um numero ou string");
-        return err;
-        // ou
-        // throw 'value deve ser um numero ou string'
-    }
+let total = 20;
+total = '40';
+function preencherDados(dados) {
+    document.body.innerHTML += `
+  <h2>${dados.nome}</h2>
+  <p>${dados.preco}</p>
+  <p>Inclue teclado? ${dados.teclado ? 'sim' : 'não'}</p>
+  `;
 }
-console.log(toNumber('200'));
+const pc = {
+    nome: 'pc',
+    preco: 22,
+    teclado: true,
+};
+preencherDados(pc);
+preencherDados({
+    nome: 'celular',
+    preco: 50,
+    teclado: false,
+});
+function pintarCategorias(value) {
+    console.log(value);
+}
+pintarCategorias('descod');
--- script.ts
+++ script.ts
@@ -1,14 +1,40 @@
-function toNumber(value: number | string){
-  if(typeof value === 'number'){
-    return value
-  } else if (typeof value === 'string'){
-    return Number(value)
-  } else{
-    const err = new Error("value deve ser um numero ou string");
-    return err
-    // ou
-    // throw 'value deve ser um numero ou string'
-  }
+type NumberOrString = string | number;
+
+let total : NumberOrString = 20
+total = '40'
+
+type Produto = {
+  nome: string;
+  preco: number;
+  teclado: boolean;
+};
+
+
+function preencherDados(dados: Produto){
+  document.body.innerHTML += `
+  <h2>${dados.nome}</h2>
+  <p>${dados.preco}</p>
+  <p>Inclue teclado? ${dados.teclado ? 'sim' : 'não'}</p>
+  `
 }
 
-console.log(toNumber('200'))
+const pc:Produto = {
+  nome: 'pc',
+  preco: 22,
+  teclado: true,
+}
+preencherDados(pc)
+
+preencherDados({
+  nome: 'celular',
+  preco: 50,
+  teclado: false,
+})
+
+type Categorias = 'design' | 'codigo' | 'descod'
+
+function pintarCategorias(value: 'design' | 'codigo' | 'descod'){
+  console.log(value)
+}
+
+pintarCategorias('descod')
