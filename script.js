961acc0189b3599ef306f930ddbf5584a6c64040
matheuspaiva1 <matheuspaiva0007@gmail.com>
2024-05-24 16:39:54 -0300

    null

--- script.js
+++ script.js
@@ -1,30 +1,7 @@
 "use strict";
-async function fetchCursos() {
-    const response = await fetch('https://api.origamid.dev/json/cursos.json');
-    const data = await response.json();
-    mostrarCursos(data);
+const button = document.querySelector('button');
+const config = localStorage.getItem('config');
+if (button !== null) {
+    button.click();
 }
-fetchCursos();
-function mostrarCursos(cursos) {
-    cursos.forEach(curso => {
-        let color;
-        if (curso.nivel === 'iniciante') {
-            color = 'blue';
-        }
-        else {
-            color = 'red';
-        }
-        document.body.innerHTML += `
-    <div>
-    <h2 style="color: ${color}">${curso.nome}</h2>
-    <p>Horas: ${curso.horas}</p>
-    <p>Aulas: ${curso.aulas}</p>
-    <p>Tipo: ${curso.gratuito ? 'Gratuito' : 'Pago'}</p>
-    <p>Tags: ${curso.tags.join(', ')}</p>
-
-
-    <strong>${curso.nivel}</strong>
-  </div>
-    `;
-    });
-}
+button?.click();
--- script.ts
+++ script.ts
@@ -1,41 +1,8 @@
-interface Curso {
-  nome: string,
-  horas: number,
-  aulas: number,
-  gratuito: boolean,
-  tags: string[],
-  idAulas: number[],
-  nivel: 'iniciante' | 'avançado',
+const button = document.querySelector('button');
+const config = localStorage.getItem('config');
+
+if (button !== null) {
+  button.click()
 }
 
-async function fetchCursos() {
-  const response = await fetch('https://api.origamid.dev/json/cursos.json');
-  const data = await response.json();
-  mostrarCursos(data);
-}
-
-fetchCursos()
-
-function mostrarCursos(cursos: Curso[]) {
-  cursos.forEach(curso => {
-    let color;
-    if(curso.nivel === 'iniciante'){
-      color = 'blue'
-    } else{
-      color = 'red'
-    }
-    document.body.innerHTML += `
-    <div>
-    <h2 style="color: ${color}">${curso.nome}</h2>
-    <p>Horas: ${curso.horas}</p>
-    <p>Aulas: ${curso.aulas}</p>
-    <p>Tipo: ${curso.gratuito ? 'Gratuito' : 'Pago'}</p>
-    <p>Tags: ${curso.tags.join(', ')}</p>
-
-
-    <strong>${curso.nivel}</strong>
-  </div>
-    `
-  })
-}
-
+button?.click()
