42c754a2a0a3cd6f2eeddf4097740380d8691924
matheuspaiva1 <matheuspaiva0007@gmail.com>
2024-06-10 15:03:43 -0300

    instanceof 3

--- script.js
+++ script.js
@@ -1,43 +1,5 @@
 "use strict";
-//comando tsc -w
-class Produto {
-    nome;
-    constructor(nome) {
-        this.nome = nome;
-    }
-}
-const livro = new Produto('guerra', 29.99);
-console.log(livro instanceof Produto);
-class Livro extends Produto {
-    autor;
-    constructor(nome, autor) {
-        super(nome);
-        this.autor = autor;
-    }
-}
-class Jogo extends Produto {
-    jogador;
-    constructor(nome, jogador) {
-        super(nome);
-        this.jogador = jogador;
-    }
-}
-function buscarProduto(buscar) {
-    if (buscar === 'hobbit') {
-        return new Livro('hobbit', 'J.R.R Tolkien');
-    }
-    if (buscar === 'Dark Souls') {
-        return new Jogo('Dark Souls', 59);
-    }
-    return null;
-}
-const produto = buscarProduto('Dark Souls');
-if (produto instanceof Livro) {
-    console.log(produto.autor);
-}
-if (produto instanceof Jogo) {
-    console.log(produto.nome);
-}
-if (produto instanceof Produto) {
-    console.log(produto.nome);
+const link = document.getElementById('origamid');
+if (link instanceof HTMLAnchorElement) {
+    link.href.replace('http://', 'https://');
 }
--- script.ts
+++ script.ts
@@ -1,53 +1,5 @@
-//comando tsc -w
-class Produto {
-  nome: string;
-  constructor(nome: string){
-    this.nome = nome;
-  }
+const link = document.getElementById('origamid')
+
+if (link instanceof HTMLAnchorElement) {
+  link.href.replace('http://', 'https://')
 }
-
-const livro = new Produto('guerra', 29.99);
-console.log(livro instanceof Produto)
-
-class Livro extends Produto {
-  autor:string;
-
-  constructor(nome:string, autor:string;){
-    super(nome)
-    this.autor = autor;
-  }
-}
-
-class Jogo extends Produto {
-  jogador:number;
-
-  constructor(nome: string,jogador:number;){
-    super(nome)
-    this.jogador = jogador;
-  }
-}
-
-function buscarProduto(buscar:string){
-  if(buscar === 'hobbit'){
-    return new Livro('hobbit','J.R.R Tolkien')
-  }
-  if (buscar === 'Dark Souls'){
-    return new Jogo('Dark Souls',59)
-  }
-  return null;
-}
-
-const produto = buscarProduto('Dark Souls')
-
-if (produto instanceof Livro){ 
-  console.log(produto.autor)
-}
-
-if (produto instanceof Jogo){ 
-  console.log(produto.nome)
-}
-
-if (produto instanceof Produto){
-  console.log(produto.nome)
-}
-
