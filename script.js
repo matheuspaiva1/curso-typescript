86aa3fbef576ba1a1ed53c0fa19642e2b94c153d
matheuspaiva1 <matheuspaiva0007@gmail.com>
2024-06-10 14:46:48 -0300

    instanceof 1

--- script.js
+++ script.js
@@ -1,7 +1,43 @@
 "use strict";
-const button = document.querySelector('button');
-const config = localStorage.getItem('config');
-if (button !== null) {
-    button.click();
+//comando tsc -w
+class Produto {
+    nome;
+    preco;
+    constructor(nome, preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+    precoReal() {
+        return `R$ ${this.preco}`;
+    }
 }
-button?.click();
+const livro = new Produto('guerra', 29.99);
+console.log(livro instanceof Produto);
+class Livro {
+    autor;
+    constructor(autor) {
+        this.autor = autor;
+    }
+}
+class Jogo {
+    jogador;
+    constructor(jogador) {
+        this.jogador = jogador;
+    }
+}
+function buscarProduto(buscar) {
+    if (buscar === 'hobbit') {
+        return new Livro('J.R.R Tolkien');
+    }
+    if (buscar === 'Dark Souls') {
+        return new Jogo(59);
+    }
+    return null;
+}
+const produto = buscarProduto('Dark Souls');
+if (produto instanceof Livro) {
+    console.log(produto.autor);
+}
+if (produto instanceof Jogo) {
+    console.log(produto.jogador);
+}
--- script.ts
+++ script.ts
@@ -1,8 +1,52 @@
-const button = document.querySelector('button');
-const config = localStorage.getItem('config');
+//comando tsc -w
+class Produto {
+  nome: string;
+  preco: number;
+  constructor(nome: string, preco: number){
+    this.nome = nome;
+    this.preco = preco;
+  }
 
-if (button !== null) {
-  button.click()
+  precoReal(){
+    return `R$ ${this.preco}`
+  }
 }
 
-button?.click()
+const livro = new Produto('guerra', 29.99);
+console.log(livro instanceof Produto)
+
+class Livro {
+  autor:string;
+
+  constructor(autor:string;){
+    this.autor = autor;
+  }
+}
+
+class Jogo {
+  jogador:number;
+
+  constructor(jogador:number;){
+    this.jogador = jogador;
+  }
+}
+
+function buscarProduto(buscar:string){
+  if(buscar === 'hobbit'){
+    return new Livro('J.R.R Tolkien')
+  }
+  if (buscar === 'Dark Souls'){
+    return new Jogo(59)
+  }
+  return null;
+}
+
+const produto = buscarProduto('Dark Souls')
+
+if (produto instanceof Livro){ 
+  console.log(produto.autor)
+}
+
+if (produto instanceof Jogo){ 
+  console.log(produto.jogador)
+}
