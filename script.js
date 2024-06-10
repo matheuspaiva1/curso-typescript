493043b38981794c745a0eed5b15a90b98542225
matheuspaiva1 <matheuspaiva0007@gmail.com>
2024-06-10 15:36:20 -0300

    interfaces DOM 3

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 "use strict";
 const links = document.querySelectorAll('.link');
+function modifACorBorder(elemento) {
+    elemento.style.color = "#D60000";
+    elemento.style.border = "1px solid #FF0F0F";
+}
 links.forEach((link) => {
-    if (link instanceof HTMLAnchorElement) {
-        console.log(link.href);
+    if (link instanceof HTMLElement) {
+        modifACorBorder(link);
     }
 });
--- script.ts
+++ script.ts
@@ -1,7 +1,12 @@
 const links = document.querySelectorAll('.link');
 
-links.forEach((link) => {
-  if (link instanceof HTMLAnchorElement) {
-    console.log(link.href);
+function modifACorBorder (elemento: HTMLElement) {
+    elemento.style.color="#D60000";
+    elemento.style.border = "1px solid #FF0F0F";
+}
+
+links.forEach((link)=>{
+  if(link instanceof HTMLElement){
+    modifACorBorder(link)
   }
 })
