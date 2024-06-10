b3ddb4aaf2c801af3b4502c41ec0874caadcb18a
matheuspaiva1 <matheuspaiva0007@gmail.com>
2024-06-10 17:32:37 -0300

    events e callbacks 2

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 "use strict";
+/**
+ const button = document.querySelector('button');
+ function handleClick(event: MouseEvent){
+   console.log(event.pageX)
+ }
+ button?.addEventListener('click', handleClick);
+ 
+ function ativarMenu(event: MouseEvent | TouchEvent | KeyboardEvent) {
+   console.log(event)
+ }
+ 
+ document.documentElement.addEventListener('mousedown', ativarMenu)
+ document.documentElement.addEventListener('touchstart', ativarMenu)
+ window.addEventListener('keydown', ativarMenu)
+*/
 const button = document.querySelector('button');
 function handleClick(event) {
-    console.log(event.pageX);
+    const elemento = event.currentTarget;
+    if (elemento instanceof HTMLElement) {
+        console.log(elemento.innerText);
+    }
 }
 button?.addEventListener('click', handleClick);
-function ativarMenu(event) {
-    console.log(event);
-}
-document.documentElement.addEventListener('mousedown', ativarMenu);
-document.documentElement.addEventListener('touchstart', ativarMenu);
-window.addEventListener('keydown', ativarMenu);
--- script.ts
+++ script.ts
@@ -1,14 +1,26 @@
-const button = document.querySelector('button');
+/**
+ const button = document.querySelector('button');
+ function handleClick(event: MouseEvent){
+   console.log(event.pageX)
+ }
+ button?.addEventListener('click', handleClick);
+ 
+ function ativarMenu(event: MouseEvent | TouchEvent | KeyboardEvent) {
+   console.log(event)
+ }
+ 
+ document.documentElement.addEventListener('mousedown', ativarMenu)
+ document.documentElement.addEventListener('touchstart', ativarMenu)
+ window.addEventListener('keydown', ativarMenu)
+*/
+
+
+const button = document.querySelector('button')
+
 function handleClick(event: MouseEvent){
-  console.log(event.pageX)
+  const elemento = event.currentTarget
+  if(elemento instanceof HTMLElement){
+    console.log(elemento.innerText)
+  }
 }
-button?.addEventListener('click', handleClick);
-
-function ativarMenu(event: MouseEvent | TouchEvent | KeyboardEvent) {
-  console.log(event)
-}
-
-document.documentElement.addEventListener('mousedown', ativarMenu)
-document.documentElement.addEventListener('touchstart', ativarMenu)
-window.addEventListener('keydown', ativarMenu)
-
+button?.addEventListener('click',handleClick)
