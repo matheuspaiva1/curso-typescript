636ef8b2a475a38ee9f324675cf5bd3ecd98719c
matheuspaiva1 <matheuspaiva0007@gmail.com>
2024-06-18 19:16:20 -0300

    unknown

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 "use strict";
 function typeGuard(value) {
     if (typeof value === 'string') {
-        return value.toLowerCase();
+        return value.toUpperCase();
     }
     if (typeof value === 'number') {
         return value.toFixed();
@@ -10,26 +10,6 @@ function typeGuard(value) {
         return value.innerText;
     }
 }
+typeGuard('origamid');
 typeGuard(200);
-typeGuard('333');
 typeGuard(document.body);
-const obj = {
-    nome: 'document'
-};
-if ('preco' in obj) {
-    console.log('sim');
-}
-async function fetchProduct() {
-    const response = await fetch('https://api.origamid.dev/json/notebook.json');
-    const json = await response.json();
-    handleProduct(json);
-}
-function handleProduct(data) {
-    console.log(data);
-    if ('total' in data) {
-        document.body.innerHTML += `
-    <p>Nome: ${data.nome}</p>
-    <p>Preco: ${data.total + 100}</p>`;
-    }
-}
-fetchProduct();
--- script.ts
+++ script.ts
@@ -1,45 +1,15 @@
-function typeGuard(value: any){
+function typeGuard(value:unknown){
   if(typeof value === 'string'){
-    return value.toLowerCase()
-  } 
-  if (typeof value === 'number'){
+    return value.toUpperCase()
+  }
+  if(typeof value === 'number'){
     return value.toFixed()
   }
-  if (value instanceof HTMLElement){
+  if(value instanceof HTMLElement){
     return value.innerText
   }
 }
 
+typeGuard('origamid')
 typeGuard(200)
-typeGuard('333')
 typeGuard(document.body)
-
-const obj = {
-  nome: 'document'
-}
-
-if('preco'in obj){
-  console.log('sim')
-}
-
-async function fetchProduct() {
-  const response = await fetch('https://api.origamid.dev/json/notebook.json')
-  const json = await response.json()
-  handleProduct(json)
-}
-
-interface Product{
-  nome:string;
-  preco:number;
-}
-
-function handleProduct(data:any) {
-  console.log(data)
-  if('total' in data) {
-    document.body.innerHTML += `
-    <p>Nome: ${data.nome}</p>
-    <p>Preco: ${data.total + 100}</p>`
-  }
-}
-
-fetchProduct()
