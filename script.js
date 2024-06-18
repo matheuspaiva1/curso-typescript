9b173c8ec330688eb9c4cd411a75b6a41b81a568
matheuspaiva1 <matheuspaiva0007@gmail.com>
2024-06-18 19:12:59 -0300

    type guard

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
 "use strict";
-function arrendondar(valor) {
-    if (typeof valor === 'number') {
-        return Math.ceil(valor);
+function typeGuard(value) {
+    if (typeof value === 'string') {
+        return value.toLowerCase();
     }
-    else {
-        return Math.ceil(Number(valor)).toString();
+    if (typeof value === 'number') {
+        return value.toFixed();
+    }
+    if (value instanceof HTMLElement) {
+        return value.innerText;
     }
 }
-console.log(arrendondar(200.32));
-console.log(arrendondar('200.32'));
+typeGuard(200);
+typeGuard('333');
+typeGuard(document.body);
+const obj = {
+    nome: 'document'
+};
+if ('preco' in obj) {
+    console.log('sim');
+}
+async function fetchProduct() {
+    const response = await fetch('https://api.origamid.dev/json/notebook.json');
+    const json = await response.json();
+    handleProduct(json);
+}
+function handleProduct(data) {
+    console.log(data);
+    if ('total' in data) {
+        document.body.innerHTML += `
+    <p>Nome: ${data.nome}</p>
+    <p>Preco: ${data.total + 100}</p>`;
+    }
+}
+fetchProduct();
--- script.ts
+++ script.ts
@@ -1,14 +1,45 @@
-function arrendondar(valor: number): number
-function arrendondar(valor: string): string 
-
-function arrendondar(valor: number | string): number | string {
-  if (typeof valor === 'number'){
-    return Math.ceil(valor)
-  } else {
-    return Math.ceil(Number(valor)).toString()
+function typeGuard(value: any){
+  if(typeof value === 'string'){
+    return value.toLowerCase()
+  } 
+  if (typeof value === 'number'){
+    return value.toFixed()
+  }
+  if (value instanceof HTMLElement){
+    return value.innerText
   }
 }
 
-console.log(arrendondar(200.32))
-console.log(arrendondar('200.32'))
+typeGuard(200)
+typeGuard('333')
+typeGuard(document.body)
 
+const obj = {
+  nome: 'document'
+}
+
+if('preco'in obj){
+  console.log('sim')
+}
+
+async function fetchProduct() {
+  const response = await fetch('https://api.origamid.dev/json/notebook.json')
+  const json = await response.json()
+  handleProduct(json)
+}
+
+interface Product{
+  nome:string;
+  preco:number;
+}
+
+function handleProduct(data:any) {
+  console.log(data)
+  if('total' in data) {
+    document.body.innerHTML += `
+    <p>Nome: ${data.nome}</p>
+    <p>Preco: ${data.total + 100}</p>`
+  }
+}
+
+fetchProduct()
